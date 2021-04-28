5ac83e4635567623ad6674d9ecf31b5defccd425
Your Name <you@example.com>
2021-04-28 23:02:24 +0430

    install phpunit

--- tests/TmpTest.php
+++ tests/TmpTest.php
@@ -3,11 +3,11 @@
 use PHPUnit\Framework\TestCase;
 
 
-
 class MyTestCases extends TestCase
 {
-    public function testSampleTests() {
-        function getChar($c)
+    public function testSampleTests()
+    {
+        function getChar($c): string
         {
             return chr($c);
         }
