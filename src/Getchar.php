88c6e0be07f7bb55c04a880fb4294bd29235e141
saeed <saeed@ddd.com>
2021-05-02 14:17:56 +0430

    ASCII_Value

--- src/Getchar.php
+++ src/Getchar.php
@@ -11,11 +11,14 @@ final class Getchar
 
 }
 
-echo getchar(74);
 
 //function getchar($c){
 //    return chr($c);
 //}
 //echo getchar(75);
+
+echo getchar(74);
+
+
 
 http://alihossein.ir/tutorials/%D8%A2%D9%85%D9%88%D8%B2%D8%B4-%D9%85%D8%AA%D8%AF-%D8%AC%D8%A7%D8%AF%D9%88%DB%8C%DB%8C-construct
