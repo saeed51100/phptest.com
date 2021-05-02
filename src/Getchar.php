94764142c2619a19c6bf0c3c30ee7b0e95f3a864
saeed <saeed@ddd.com>
2021-05-02 14:16:16 +0430

    ASCII_Value

--- src/Getchar.php
+++ src/Getchar.php
@@ -17,3 +17,5 @@ echo getchar(74);
 //    return chr($c);
 //}
 //echo getchar(75);
+
+http://alihossein.ir/tutorials/%D8%A2%D9%85%D9%88%D8%B2%D8%B4-%D9%85%D8%AA%D8%AF-%D8%AC%D8%A7%D8%AF%D9%88%DB%8C%DB%8C-construct
