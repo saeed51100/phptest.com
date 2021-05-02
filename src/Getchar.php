0f0a08120123b555dbf5ae931ede429c96c0b378
saeed <saeed@ddd.com>
2021-05-02 13:57:07 +0430

    ASCII_Value

--- src/Getchar.php
+++ src/Getchar.php
@@ -4,11 +4,16 @@ final class Getchar
 {
     private $getchar;
 
-    private function __construct (getchar)
+    private function __construct (string $getchar)
     {
-        return chr($c);
+        return chr();
     }
-
 
 }
 
+echo getchar(74);
+
+function getchar($c){
+    return chr($c);
+}
+echo getchar(75);
