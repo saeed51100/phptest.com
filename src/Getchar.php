0b0de26bf64f1d925ef2169776571fdd49cad8fc
saeed <saeed@ddd.com>
2021-05-03 08:09:23 +0430

    ASCII_Value

--- src/Getchar.php
+++ src/Getchar.php
@@ -1,28 +1,18 @@
 <?php
-//
-//final class Getchar
-//{
-//    private $getchar;
-//
-//    private function __construct($c)
-//    {
-//        $this->getchar = $getchar;
-//    }
-//
-//    function getchar($c): string
-//    {
-//        return chr($c);
-//
-//    }
-//}
-//
-//$Getchar = new Getchar();
 
-//$getchar = new Getchar();
-//echo $getchar->getchar();
+class Fruit {
+    public $name;
 
+    function __construct($name) {
+        $this->name = chr($name);
+    }
+    function get_name() {
+        return $this->name;
+    }
+}
 
-
+$apple = new Fruit(74);
+echo $apple->get_name();
 
 
 
@@ -30,20 +20,7 @@
 //function getchar($c){
 //    return chr($c);
 //}
-//echo getchar(75);
 
 
-class Fruit {
-    public $name;
 
-    function __construct($name) {
-        $this->name = $name;
-    }
-    function get_name() {
-        return $this->name;
-    }
-}
-
-$apple = new Fruit("Apple");
-echo $apple->get_name();
 
