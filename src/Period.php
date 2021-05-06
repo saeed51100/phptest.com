69d4fcf9c7ccbe9712840acc75ffbde809ecac59
Your Name <you@example.com>
2021-05-06 22:23:44 +0430

    Period

--- src/Period.php
+++ src/Period.php
@@ -1,22 +1,42 @@
 <?php
 namespace saeed;
 
-class Period
-{
-   public function periodIsLate($last, $today, $cycleLength): bool
-   {
-//       return false;
-       return $last;
+//class Period
+//{
+//   public function periodIsLate($last, $today, $cycleLength): bool
+//   {
+////       return false;
+//       return $last;
+//
+//   }
+//}
+//
+//echo (new Period())->periodIsLate(2016-07-13,2016-07-15,5);
 
-   }
-}
+// date in php
+echo  date("Y/m/d") . "<br>". "<br>"; // 2021/05/06
 
-echo (new Period())->periodIsLate(2016-07-13,2016-07-15,5);
+echo  date("y/m/d") . "<br>". "<br>"; // 21/05/06
+echo  date("Y.m.d") . "<br>". "<br>"; // 2021.05.06
+echo  date("Y-m-d") . "<br>". "<br>"; // 2021-05-06
+echo  date("Y M D"). "<br>". "<br>"; // 2021 May Thu
+echo  date("l"). "<br>". "<br>"; // Thursday
+echo  date("F"). "<br>". "<br>"; // May
+echo  date("h:i:sa"). "<br>". "<br>"; // 05:38:54pm
+echo  date("g:i:s a"). "<br>". "<br>"; // 5:38:54 pm
 
+echo  time(). "<br>". "<br>"; // 1620322734
 
+echo  date("N"). "<br>". "<br>"; // 4
+echo  date("w"). "<br>". "<br>"; // 4
 
+echo  date("z"). "<br>". "<br>"; // 125
+echo  date("W"). "<br>". "<br>"; // 18
+echo  date("t"). "<br>". "<br>"; // 31
+echo  date("L"). "<br>". "<br>"; // 0
 
-
+echo  date("e"). "<br>". "<br>"; // UTC
+echo  date("P"). "<br>". "<br>"; //+00:00
 
 
 
