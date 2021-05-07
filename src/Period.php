a5513179f78f9af74d8e33f4aafcea9dcd1f81c3
Your Name <you@example.com>
2021-05-07 13:13:58 +0430

    Period

--- src/Period.php
+++ src/Period.php
@@ -6,19 +6,18 @@ class Period
    public function periodIsLate($last, $today, $cycleLength): bool
    {
 
-       $last = 2021-05-01;
-       $today = 2021-05-15;
-       $cycleLength = time($today)-time($last);
-       return $cycleLength;
-
+       if ($cycleLength>($today-$last)){
+           return false;
+       }else{
+           return true;
+       }
 //       return false;
 
    }
 }
 
-//echo (new Period())->periodIsLate(2016-07-13,2016-07-15,5);
+echo (new Period())->periodIsLate(2016-07-13,2016-07-15,5);
 
-echo (new Period())->periodIsLate($cycleLength=45);
 
 
 
