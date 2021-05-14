bd7053d39c2810fb0205a69772ff9ea1df77d2d0
Your Name <you@example.com>
2021-05-14 20:00:37 +0430

    Exclamation2

--- tests/Exclamation2Test.php
+++ tests/Exclamation2Test.php
@@ -4,20 +4,20 @@ use PHPUnit\Framework\TestCase;
 
 class Exclamation2Test extends TestCase
 {
-    protected $Exclamation;
+    protected $Exclamation2;
 
     public function setUp(): void
     {
-        $this->Exclamation = new \saeed\Exclamation2();
+        $this->Exclamation2 = new \saeed\Exclamation2();
     }
 
     public function testDescriptionExamples() {
-        $this->assertEquals("Hi", remove("Hi!"));
-        $this->assertEquals("Hi", remove("Hi!!!"));
-        $this->assertEquals("!Hi", remove("!Hi"));
-        $this->assertEquals("!Hi", remove("!Hi!"));
-        $this->assertEquals("Hi! Hi", remove("Hi! Hi!"));
-        $this->assertEquals("Hi", remove("Hi"));
+        $this->assertEquals("Hi", $this->Exclamation2->remove("Hi!"));
+        $this->assertEquals("Hi", $this->Exclamation2->remove("Hi!!!"));
+        $this->assertEquals("!Hi", $this->Exclamation2->remove("!Hi"));
+        $this->assertEquals("!Hi", $this->Exclamation2->remove("!Hi!"));
+        $this->assertEquals("Hi! Hi", $this->Exclamation2->remove("Hi! Hi!"));
+        $this->assertEquals("Hi", $this->Exclamation2->remove("Hi"));
     }
 }
 
