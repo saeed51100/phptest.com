a253f55917ca4360315368922363ea7cc355d56e
Your Name <you@example.com>
2021-05-14 18:43:17 +0430

    Exclamation2

--- src/Exclamation2.php
+++ src/Exclamation2.php
@@ -5,11 +5,11 @@ class Exclamation2
 {
     public function remove(string $s): string {
         // Coding and coding ...
-        $string = preg_replace('/!+$/', '', $string);
+        return preg_replace('/!+$/', '', $s);
     }
 }
 
-//echo (new Exclamation2())->periodIsLate(2016-07-13,2015-07-15,5);
+echo (new Exclamation2())->remove("!hello!!!!");
 
 
 
