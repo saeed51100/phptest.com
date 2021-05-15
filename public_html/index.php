f10d827f81c2bf74b14598b71121ce90c3dc9ea4
saeed <saeed@ddd.com>
2021-05-15 13:24:14 +0430

    Array.diff

--- public_html/index.php
+++ public_html/index.php
@@ -1,2 +1,2 @@
 <?php
-require '../src/Array.diff.php';
+require '../src/Arraydiff.php';
---
+++ src/Arraydiff.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace saeed;
+
+class diff
+{
+    public function arrayDiff($a, $b) {
+        // your code here
+//        return array_diff($a, $b);
+    }
+}
+
+//echo (new diff())->remove("!hello!!!!");
+
+
+
+
+
+
+
+
+
+
