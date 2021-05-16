5567474d353a44f8207533c3264f2ba2bd7d1aff
saeed <saeed@ddd.com>
2021-05-16 10:39:02 +0430

    Array.diff

--- tests/ArraydiffTest.php
+++ tests/ArraydiffTest.php
@@ -11,15 +11,14 @@ class diffTest extends TestCase
         $this->Exclamation2 = new \saeed\Arraydiff();
     }
 
-    public function testDescriptionExamples() {
-        $this->assertEquals("Hi", $this->Exclamation2->remove("Hi!"));
-        $this->assertEquals("Hi", $this->Exclamation2->remove("Hi!!!"));
-        $this->assertEquals("!Hi", $this->Exclamation2->remove("!Hi"));
-        $this->assertEquals("!Hi", $this->Exclamation2->remove("!Hi!"));
-        $this->assertEquals("Hi! Hi", $this->Exclamation2->remove("Hi! Hi!"));
-        $this->assertEquals("Hi", $this->Exclamation2->remove("Hi"));
+    public function testSampleTests() {
+        $this->assertEquals([2], $this->Exclamation2->arrayDiff([1,2], [1]), "a was [1,2], b was [1], expected [2]");
+        $this->assertEquals([2,2], $this->Exclamation2->arrayDiff([1,2,2], [1]), "a was [1,2,2], b was [1], expected [2,2]");
+        $this->assertEquals([1], $this->Exclamation2->arrayDiff([1,2,2], [2]), "a was [1,2,2], b was [2], expected [1]");
+        $this->assertEquals([1,2,2], $this->Exclamation2->arrayDiff([1,2,2], []), "a was [1,2,2], b was [], expected [1,2,2]");
+        $this->assertEquals([], $this->Exclamation2->arrayDiff([], [1,2]), "a was [], b was [1,2], expected []");
+        $this->assertEquals([3], $this->Exclamation2->arrayDiff([1, 2, 3], [1,2]), "a was [1, 2, 3], b was [1,2], expected [3]");
     }
 }
-
 
 
