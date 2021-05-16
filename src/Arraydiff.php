1b3a8d92d846f099217b05eb78bf906a7620081c
saeed <saeed@ddd.com>
2021-05-16 09:46:19 +0430

    Array.diff

--- src/Arraydiff.php
+++ src/Arraydiff.php
@@ -10,8 +10,8 @@ class Arraydiff
     }
 }
 
-// The Echo command does not have the ability to display array values
-// Use print_r instead of echo for print array
+// The Echo command does not have the ability to display array values.
+// Use print_r instead of echo for print array.
 print_r((new Arraydiff)->arrayDiff([1,2,2,2,3],[2]));
 
 
