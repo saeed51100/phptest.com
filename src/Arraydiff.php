7bec0604776a9124e09251186331d2205ba1e5ed
Your Name <you@example.com>
2021-05-16 21:57:36 +0430

    array_diff

--- src/Arraydiff.php
+++ src/Arraydiff.php
@@ -12,8 +12,9 @@ class Arraydiff
 
 // The Echo command does not have the ability to display array values.
 // Use print_r instead of echo for print array.
-print_r((new Arraydiff)->arrayDiff([1,2,2,2,3],[2]));
-
+echo "<pre>";
+print_r((new Arraydiff)->arrayDiff([1,2,2,2,3,3],[2]));
+echo "</pre>";
 
 
 
