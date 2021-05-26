f8c9601d6bcce9027ce7026d0f29e0d5e03cf68f
Your Name <you@example.com>
2021-05-26 20:34:22 +0430

    Convertnumber

--- tests/ConvertnumberTest.php
+++ tests/ConvertnumberTest.php
@@ -6,31 +6,23 @@ use PHPUnit\Framework\TestCase;
 
 class ConvertnumberTest extends TestCase
 {
-    protected $Reversewords;
-
+    protected $Convertnumber;
 
 
     public function setUp(): void
     {
-        $this->Reversewords = new Reversewords();
+        $this->Convertnumber = new Convertnumber();
     }
-    public function testBasicTests() {
-        describe("Tests", () => {
-            it("test", () => {
-                Test.assertDeepEquals(digitize(35231),[1,3,2,5,3]);
 
-            });
-});
+    public function testBasicTests()
+    {
+
+        $this->assertDeepEquals([1, 3, 2, 5, 3], $this->Convertnumber->digitize(35231));
+
     }
 
 }
 
 
-$this->assertEquals('elppa', $this->Convertnumber-> reverse('apple'));
+$this->assertEquals('elppa', $this->Convertnumber->reverse('apple'));
 
-//describe("Tests", () => {
-//    it("test", () => {
-//        Test.assertDeepEquals(digitize(35231),[1,3,2,5,3]);
-//
-//    });
-//});
