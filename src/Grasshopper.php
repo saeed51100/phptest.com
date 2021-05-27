7b5fd351257183379fa0276d7d39fda3d36cfd7b
Your Name <you@example.com>
2021-05-27 10:14:23 +0430

    Convertnumber

--- src/Grasshopper.php
+++ src/Grasshopper.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace saeed;
 
 
@@ -7,13 +8,14 @@ class Grasshopper
 
     public function summation($c)
     {
-        return ($c * ($c + 1) / 2);
+        // I find this in internet:
+        // return ($c * ($c + 1) / 2);
+
+        return array_sum(range(1, $c));
     }
 }
 
 
-
-
 echo "<h2>";
-echo(new Grasshopper)->summation (80) ;
+echo (new Grasshopper)->summation(80);
 echo "</h2>";
