d395da52af5bd55d1c8f1f743413a92224e2a236
Your Name <you@example.com>
2021-05-28 22:07:17 +0430

    Convertnumber

--- src/Substitutingvariables.php
+++ src/Substitutingvariables.php
@@ -9,6 +9,7 @@ class Substitutingvariables
     {   // I find this:
         // https://www.delftstack.com/howto/php/how-to-properly-format-a-number-with-leading-zeros-in-php/
         // return "Value is " . str_pad($c, 5, "0", STR_PAD_LEFT);
+
         return sprintf("Value is %05d",$c); // This in codewars.
 
     }
