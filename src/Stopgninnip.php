2b7acc65ab30f6ccfb5d6151a6e57a33328a94b2
Your Name <you@example.com>
2021-05-30 23:20:22 +0430

    Quarteroftheyear

--- src/Stopgninnip.php
+++ src/Stopgninnip.php
@@ -6,12 +6,25 @@ namespace saeed;
 class Stopgninnip
 {
 
-    public function quarterOf($month)
-    {
-        return ceil($month / 3);
+    public function spinWords(string $str): string {
 
-    }
+
+//        https://minwook-shin.github.io/how-to-solve-stop-gninnips-my-sdrow!/
+//        public String spinWords(String sentence) {
+//        String[] arr = sentence.split(" ");
+//        for(int i=0;i<arr.length;i++){
+//            if(arr[i].length() >= 5)
+//                arr[i] = new StringBuffer(arr[i]).reverse().toString();
+//        }
+//        return String.join(" ",arr);
+//    }
+
+        preg_replace_callback('/(\b\w{5,}\b)+/i', fn($str) => strrev($str[0]), 'Hello world, this is a test string.');
+
+
+
+
 }
 
-echo (new Stopgninnip)->quarterOf(7);
+//echo (new Stopgninnip)->quarterOf(7);
 
