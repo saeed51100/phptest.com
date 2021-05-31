8be06383acd979d65ef15e72c33b62a61be1b4e2
Your Name <you@example.com>
2021-05-31 20:14:03 +0430

    Quarteroftheyear

--- src/Stopgninnip.php
+++ src/Stopgninnip.php
@@ -13,9 +13,16 @@ class Stopgninnip
         // return preg_replace('/(\b\w{5,}\b)+/i', "1111111", $str);
 
 
-        // preg_replace_callback example:
-        return preg_replace_callback('/(\b\w{5,}\b)+/i', function ($match) {
-            return strrev($match[0]);
+        // preg_replace_callback example: (My solution)
+        // return preg_replace_callback('/(\b\w{5,}\b)+/i',
+        //     function ($match) {
+        //         return strrev($match[0]);
+        //     }, $str);
+
+
+        // codewars solution:
+        return preg_replace_callback('/\w{5,}/', function ($matches) {
+            return strrev($matches[0]);
         }, $str);
 
 
