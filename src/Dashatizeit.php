430038f98a1eeddd7aee1b92f04d38cff389a2dd
Your Name <you@example.com>
2021-06-01 19:00:11 +0430

    Quarteroftheyear

--- src/Dashatizeit.php
+++ src/Dashatizeit.php
@@ -7,10 +7,12 @@ class Dashatizeit
 {
 
     public function dashatize(int $num): string {
-        // Replace with your code
+        return preg_replace_callback('/\w{5,}/', function ($matches) {
+            return strrev($matches[0]);
+        }, $str);
     }
 
 }
 
-echo (new Dashatizeit)->spinWords(" is currently unable to handle this request.");
+echo (new Dashatizeit)->dashatize(" is currently unable to handle this request.");
 
--- tests/DashatizeitTest.php
+++ tests/DashatizeitTest.php
@@ -6,22 +6,19 @@ use PHPUnit\Framework\TestCase;
 
 class DashatizeitTest extends TestCase
 {
-    protected $Stopgninnip;
+    protected $Dashatizeit;
 
     public function setUp(): void
     {
-        $this->Stopgninnip = new Dashatizeit();
+        $this->Dashatizeit = new Dashatizeit();
     }
 
     public function testSampleTests()
     {
-        $this->assertEquals("emocleW", $this->Stopgninnip->spinWords("Welcome"));
-        $this->assertEquals("Hey wollef sroirraw", $this->Stopgninnip->spinWords("Hey fellow warriors"));
-        $this->assertEquals("This is a test", $this->Stopgninnip->spinWords("This is a test"));
-        $this->assertEquals("This is rehtona test", $this->Stopgninnip->spinWords("This is another test"));
-        $this->assertEquals("You are tsomla to the last test", $this->Stopgninnip->spinWords("You are almost to the last test"));
-        $this->assertEquals("Just gniddik ereht is llits one more", $this->Stopgninnip->spinWords("Just kidding there is still one more"));
-        $this->assertEquals("ylsuoireS this is the last one", $this->Stopgninnip->spinWords("Seriously this is the last one"));
+        $this->assertEquals('2-7-4', $this->Dashatizeit->dashatize(274));
+        $this->assertEquals('5-3-1-1', $this->Dashatizeit->dashatize(5311));
+        $this->assertEquals('86-3-20', $this->Dashatizeit->dashatize(86320));
+        $this->assertEquals('9-7-4-3-02', $this->Dashatizeit->dashatize(974302));
 
     }
 }
