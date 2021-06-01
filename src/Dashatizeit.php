1ce5a2e095c51876174da02171e4f81ebd080c85
Your Name <you@example.com>
2021-06-01 19:08:20 +0430

    Quarteroftheyear

--- src/Dashatizeit.php
+++ src/Dashatizeit.php
@@ -7,12 +7,12 @@ class Dashatizeit
 {
 
     public function dashatize(int $num): string {
-        return preg_replace_callback('/\w{5,}/', function ($matches) {
-            return strrev($matches[0]);
-        }, $str);
+        return preg_replace_callback('/\d/', function ($matches) {
+            return 6;
+        }, $num);
     }
 
 }
 
-echo (new Dashatizeit)->dashatize(" is currently unable to handle this request.");
+echo (new Dashatizeit)->dashatize(646516132);
 
