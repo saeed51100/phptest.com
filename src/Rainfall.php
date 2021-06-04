8dd7fab97bb2afe758100e141468438b1dfca83f
Your Name <you@example.com>
2021-06-04 14:12:47 +0430

    Quarteroftheyear

--- src/Rainfall.php
+++ src/Rainfall.php
@@ -6,21 +6,22 @@ use phpDocumentor\Reflection\DocBlock\Tags\Return_;
 
 class Rainfall
 {
-    public function solution( $strng )
+    public function solution($strng)
     {
 
 
+        $nnn= explode(" ",$strng);
+//       return array_chunk($nnn,13);
+       return ($nnn);
 
-
-        return /*array_values*/(explode('\n', $strng));
 
 
 
     }
 
 
-
 }
+
 $data =
     "Rome:Jan 81.2,Feb 63.2,Mar 70.3,Apr 55.7,May 53.0,Jun 36.4,Jul 17.5,Aug 27.5,Sep 60.9,Oct 117.7,Nov 111.0,Dec 97.9\n"
     . "London:Jan 48.0,Feb 38.9,Mar 39.9,Apr 42.2,May 47.3,Jun 52.1,Jul 59.5,Aug 57.2,Sep 55.4,Oct 62.0,Nov 59.0,Dec 52.9\n"
