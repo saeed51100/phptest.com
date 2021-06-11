c91d5159340ff22a47f726b837558ac26ec83c55
Your Name <you@example.com>
2021-06-12 00:01:05 +0430

    Quarteroftheyear

--- tests/MultiplyTest.php
+++ tests/MultiplyTest.php
@@ -1,6 +1,24 @@
 <?php
+
 use saeed\Multiply;
 use PHPUnit\Framework\TestCase;
+
+//class MultiplyTest extends TestCase
+//{
+//    protected $Multiply;
+//
+//    public function setUp(): void
+//    {
+//        $this->Multiply = new \saeed\Multiply();
+//    }
+//
+//    public function testThatSomethingShouldHappen()
+//    {
+//        $this->assertEquals("a", "a");
+//        $this->assertEquals([0], [0]);
+//    }
+//}
+
 
 class MultiplyTest extends TestCase
 {
@@ -11,32 +29,20 @@ class MultiplyTest extends TestCase
         $this->Multiply = new \saeed\Multiply();
     }
 
-    public function testThatSomethingShouldHappen()
+    public function testBasics()
     {
-        $this->assertEquals("a", "a");
-        $this->assertEquals([0], [0]);
+        $this->assertEquals(1, $this->Multiply->multiply(1, 1));
+        $this->assertEquals(6, $this->Multiply->multiply(2, 3));
+        $this->assertEquals(15, $this->Multiply->multiply(3, 5));
+        $this->assertEquals(15, $this->Multiply->multiply(5, 3));
+    }
+
+    public function testRandom()
+    {
+        for ($i = 0; $i < 100; $i++) {
+            $a = 1 + ~~(20 * lcg_value());
+            $b = 1 + ~~(20 * lcg_value());
+            $this->assertEquals($this->Multiply->multiply($a, $b), $a * $b);
+        }
     }
 }
-
-
-//class MultiplyTest extends TestCase {
-//        protected $Multiply;
-//
-//    public function setUp(): void
-//    {
-//        $this->Multiply = new \saeed\Multiply();
-//    }
-//    public function testBasics() {
-//        $this->assertEquals(multiply(1, 1), 1);
-//        $this->assertEquals(multiply(2, 3), 6);
-//        $this->assertEquals(multiply(3, 5), 15);
-//        $this->assertEquals(multiply(5, 3), 15);
-//    }
-//    public function testRandom() {
-//        for ($i = 0; $i < 100; $i++) {
-//            $a = 1 + ~~(20 * lcg_value());
-//            $b = 1 + ~~(20 * lcg_value());
-//            $this->assertEquals(multiply($a, $b), $a * $b);
-//        }
-//    }
-//}
