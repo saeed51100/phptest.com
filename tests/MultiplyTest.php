51c728a8f4399895e52af2fc7851aa13e3c5a0ac
Your Name <you@example.com>
2021-06-11 23:40:53 +0430

    Quarteroftheyear

--- tests/MultiplyTest.php
+++ tests/MultiplyTest.php
@@ -1,5 +1,4 @@
 <?php
-
 use saeed\Multiply;
 use PHPUnit\Framework\TestCase;
 
@@ -20,4 +19,24 @@ class MultiplyTest extends TestCase
 }
 
 
-
+//class MultiplyTest extends TestCase {
+//        protected $Multiply;
+//
+//    public function setUp(): void
+//    {
+//        $this->Multiply = new \saeed\Multiply();
+//    }
+//    public function testBasics() {
+//        $this->assertEquals(multiply(1, 1), 1);
+//        $this->assertEquals(multiply(2, 3), 6);
+//        $this->assertEquals(multiply(3, 5), 15);
+//        $this->assertEquals(multiply(5, 3), 15);
+//    }
+//    public function testRandom() {
+//        for ($i = 0; $i < 100; $i++) {
+//            $a = 1 + ~~(20 * lcg_value());
+//            $b = 1 + ~~(20 * lcg_value());
+//            $this->assertEquals(multiply($a, $b), $a * $b);
+//        }
+//    }
+//}
