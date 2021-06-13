a3d6ae83e75ee3e412595a5498ab3970330563fe
Your Name <you@example.com>
2021-06-13 21:44:41 +0430

    Quarteroftheyear

--- src/MatrixAddition.php
+++ src/MatrixAddition.php
@@ -9,6 +9,7 @@ class MatrixAddition
 {
     function myfunction($ar1, $ar2)
     {
+        // https://stackoverflow.com/questions/1496682/how-to-sum-all-column-values-in-multi-dimensional-array?noredirect=1&lq=1
         $sums = array();
         foreach (array_keys($ar1 + $ar2) as $key) {
             $sums[$key] = (isset($ar1[$key]) ? $ar1[$key] : 0) + (isset($ar2[$key]) ? $ar2[$key] : 0);
@@ -17,16 +18,14 @@ class MatrixAddition
     }
 
 
-
 }
+
 $a = array(10, 2, 5, 10, 0);
 $b = array(1, 20, 11, 8, 3);
 echo "<pre>";
-   print_r((new MatrixAddition)->myfunction($a, $b));
+print_r((new MatrixAddition)->myfunction($a, $b));
 echo "</pre>";
 
 
 
 
-// https://stackoverflow.com/questions/1496682/how-to-sum-all-column-values-in-multi-dimensional-array?noredirect=1&lq=1
-//https://backendbaz.ir/php-array-func-array-map/
