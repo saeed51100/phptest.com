b6ccc621626e237a77b861b3276b660b13d4393b
Your Name <you@example.com>
2021-06-23 22:51:21 +0430

    Quarteroftheyear

--- src/Moleculetoatoms.php
+++ src/Moleculetoatoms.php
@@ -5,13 +5,27 @@ namespace saeed;
 
 class Moleculetoatoms
 {
-    function parse_molecule(string $formula): array
+    function parse_molecule(string $formula)
     {
         $nnn = preg_replace_callback('/[a-zA-Z]|\(|\)|[0-9]/', function ($matches) {
             return "  " . $matches[0];
         }, $formula);
         $yyy = ltrim($nnn);
-        return explode("  ", $yyy);
+        $mmm = explode("  ", $yyy);
+
+//        return $mmm;
+
+
+        for ($row = 0; $row < count($mmm); $row++) {
+            for ($col = 0; $col < 2; $col++) {
+
+                $result[$row][0] = ($ar1[$row][$col]) + ($ar2[$row][$col]);
+                $result[$row][1] = ($ar1[$row][$col]) + ($ar2[$row][$col]);
+
+            }
+
+        }
+
 
     }
 
