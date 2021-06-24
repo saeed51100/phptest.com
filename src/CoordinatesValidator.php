2ee68bb84bf9fb99197e1c2f94e82a4ac765ffe4
Your Name <you@example.com>
2021-06-24 22:14:12 +0430

    Quarteroftheyear

--- src/CoordinatesValidator.php
+++ src/CoordinatesValidator.php
@@ -5,15 +5,16 @@ namespace saeed;
 
 class CoordinatesValidator
 {
-    function isValidCoordinates($coordinates) {
+    function isValidCoordinates($coordinates)
+    {
 
-        return true;
+       return preg_match("/[0-9]*\.[0-9]+, [0-9]*\.[0-9]/", $coordinates);
+
     }
 }
 
 
 $a = "24.53525235, 23.45235";
-
 
 
 echo "<pre>";
