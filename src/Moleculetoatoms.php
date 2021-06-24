7127137aa10a8c73703ee8641f255f75369de34d
Your Name <you@example.com>
2021-06-24 18:53:26 +0430

    Quarteroftheyear

--- src/Moleculetoatoms.php
+++ src/Moleculetoatoms.php
@@ -13,19 +13,27 @@ class Moleculetoatoms
         $yyy = ltrim($nnn);
         $mmm = explode("  ", $yyy);
 
-        return $mmm;
+//        return $mmm;
 
 
-//        for ($row = 0; $row < count($mmm); $row++) {
-//            for ($col = 0; $col < 2; $col++) {
-//
-//                $result[$row][0] = ($mmm[$row]);
-//                $result[$row][1] = (1);
-//
-//            }
-//
-//        }
-//        return $result;
+        for ($row = 0; $row < count($mmm); $row++) {
+            for ($col = 0; $col < 2; $col++) {
+
+
+                $result[$row][0] = ($mmm[$row]);
+
+                if (3 > 7) {
+                    $zarib = 3;
+                } else {
+                    $zarib = 9;
+                }
+
+                $result[$row][1] = ($zarib);
+
+            }
+
+        }
+        return $result;
 
 
     }
