6559acd490b2cb83dfef17529124af764bc69090
Your Name <you@example.com>
2021-06-25 22:59:09 +0430

    Quarteroftheyear

--- src/Weightforweight.php
+++ src/Weightforweight.php
@@ -11,20 +11,24 @@ class Weightforweight
 
         for ($row = 0; $row < count($mmm); $row++) {
 
-        // Get the sum of digits in PHP
-        // https://stackoverflow.com/questions/3232511/get-the-sum-of-digits-in-php
-                $queu = array_sum(str_split($mmm[$row]));
+            // Get the sum of digits in PHP
+            // https://stackoverflow.com/questions/3232511/get-the-sum-of-digits-in-php
+            $queu = array_sum(str_split($mmm[$row]));
 
 
-                $asum[$row] = ($queu);
+            $asum[$row] = ($queu);
 
 
         }
         // PHP - Merge two arrays (same-length) into one associative?
         // https://stackoverflow.com/questions/1200885/php-merge-two-arrays-same-length-into-one-associative
-        return array_combine($mmm,$asum);
-//        return $mmm;
+        $tt = array_combine($mmm, $asum);
+//        return $tt;
 
+
+        asort($tt);
+
+        return $tt;
 
 
     }
