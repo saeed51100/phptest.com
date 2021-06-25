d5c52b8200536855a03d64b4f45c2d6ab7f11518
Your Name <you@example.com>
2021-06-25 19:32:14 +0430

    Quarteroftheyear

--- src/Weightforweight.php
+++ src/Weightforweight.php
@@ -8,23 +8,29 @@ class Weightforweight
     function sortVazn($vazn)
     {
         $mmm = explode(" ", $vazn);
-        return $mmm;
-
-//        $mmm[0] = rtrim($mmm[0], ",");
-
-     
+//        return $mmm;
 
 
+        for ($row = 0; $row < count($mmm); $row++) {
+            for ($col = 0; $col < 2; $col++) {
 
 
+                $result[$row][0] = ($mmm[$row]);
+// Get the sum of digits in PHP
+// https://stackoverflow.com/questions/3232511/get-the-sum-of-digits-in-php
+                $zarib = array_sum(str_split($result[$row][0]));
 
 
+                $result[$row][1] = ($zarib);
+
+            }
 
 
+        }
+        return $result;
 
     }
 }
-
 
 
 $a = "56 65 74 100 99 68 86 180 90";
