eaaafd1f74aa0724419d56a0e2af9228047ab4c6
Your Name <you@example.com>
2021-06-25 12:46:08 +0430

    Quarteroftheyear

--- src/CoordinatesValidator.php
+++ src/CoordinatesValidator.php
@@ -8,21 +8,23 @@ class CoordinatesValidator
     function isValidCoordinates($coordinates)
     {
         $mmm = explode(" ", $coordinates);
-//        return rtrim ($mmm[0],",");
-
-        if ((preg_match("/[0-9]*\.[0-9]+, [0-9]*\.[0-9]/", $coordinates))
+//return rtrim ($mmm[0],",");
+//var_dump($mmm[1]);
+        if (/*(preg_match("/[0-9]*\.[0-9]+, [0-9]*\.[0-9]/", $coordinates))*/
+            is_float($mmm[0])
+            && is_float($mmm[1])
             && ($mmm[0] >= -90 && $mmm[0] <= 90)
-            && ($mmm[1] >= -180 && $mmm[1] <= 180)) {
-            return "true";
-        } else {
-            return "false";
-        }
+            && ($mmm[1] >= -180 && $mmm[1] <= 180))
+            return true;
+        else
+            return false;
 
     }
 }
 
 
-$a = "55.53525235, 23.45235";
+//$a = "-23, 25";
+$a = "24.53525235, 23.45235";
 
 
 echo "<pre>";
