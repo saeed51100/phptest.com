39126b0e417cf8dec281897ad6ace0843c0548f2
Your Name <you@example.com>
2021-06-25 14:46:11 +0430

    Quarteroftheyear

--- src/CoordinatesValidator.php
+++ src/CoordinatesValidator.php
@@ -15,6 +15,7 @@ class CoordinatesValidator
             && is_numeric($mmm[1])
             && ($mmm[0] >= -90 && $mmm[0] <= 90)
             && ($mmm[1] >= -180 && $mmm[1] <= 180)
+            !
         )
             return true;
         else
