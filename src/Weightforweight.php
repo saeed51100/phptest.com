6ee553f5cff948a06f646a62d3186f10cc34266a
saeed <saeed@ddd.com>
2021-06-27 11:44:40 +0430

    Array.diff

--- src/Weightforweight.php
+++ src/Weightforweight.php
@@ -10,6 +10,8 @@ class Weightforweight
     {
         $mmm = explode(" ", $str);
 //        return $mmm;
+        return sort($mmm);
+//        return $mmm;
         for ($row = 0; $row < count($mmm); $row++) {
             for ($col = 0; $col < 2; $col++) {
                 // Get the sum of digits in PHP
@@ -25,14 +27,14 @@ class Weightforweight
         // sort two dimensional array by column.
         // https://stackoverflow.com/questions/2426917/how-do-i-sort-a-multidimensional-array-by-one-of-the-fields-of-the-inner-array-i
         $col = array_column($queu, "1");
-
         array_multisort($col, SORT_ASC, $queu);
-        return $queu;
+//        return $queu;
 
-
-        // implode array with keys.
-        // https://stackoverflow.com/questions/3233896/implode-array-of-values-as-well-as-its-keys
-        return implode(" ", array_keys($queu));
+        // implode two dimensional array by column.
+        // https://stackoverflow.com/questions/16710800/return-single-column-from-a-multi-dimensional-array
+        return implode(" ", array_map(function ($entry) {
+            return $entry["0"];
+        }, $queu));
 
 
     }
@@ -49,7 +51,3 @@ echo "</pre>";
 
 
 
-
-// Get the sum of digits in PHP
-// https://stackoverflow.com/questions/3232511/get-the-sum-of-digits-in-php
-// $queu = array_sum(str_split($result[$row]));
