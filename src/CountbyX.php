bd0ff90650aca4d28a72e866ded461dd8c1ceac6
Your Name <you@example.com>
2021-07-07 20:09:36 +0430

    Quarteroftheyear

--- src/CountbyX.php
+++ src/CountbyX.php
@@ -7,11 +7,24 @@ class CountbyX
 
     function countBy($x, $n)
     {
+        // My solution:
         for ($i = 0; $i < $n; $i++) {
 
             $result[$i] = ($i + 1) * $x;
         }
         return $result;
+
+
+        // Codewars solution1:
+        // return range($x, $n * $x, $x);
+
+
+        // Codewars solution2:
+        // $result = [$x];
+        // for ($i = 1; $i < $n; $i++) {
+        //     array_push($result, $x * ($i + 1));
+        // }
+        // return $result;
 
 
     }
