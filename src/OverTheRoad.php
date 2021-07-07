0ab338b6fa649e4c5de7bba79b4d239db82acf8d
Your Name <you@example.com>
2021-07-07 23:46:13 +0430

    Quarteroftheyear

--- src/OverTheRoad.php
+++ src/OverTheRoad.php
@@ -5,27 +5,12 @@ namespace saeed;
 class OverTheRoad
 {
 
-    function past($h, $m, $s)
-    {
-        // My solution:
-        if ($h >= 0 && $h <= 23 && $m >= 0 && $m <= 59 && $s >= 0 && $s <= 59) {
-            $result = ($h * 3600 + $m * 60 + $s) * 1000;
-        } else return "Enter a valid time!";
-
-        return $result;
-
-
-        // Codewars solution1:
-        // return (($h * 60 * 60) + $m * 60 + $s) * 1000;
-
-
-        // Codewars solution2:
-        // return strtotime("$h:$m:$s", 0) * 1000;
-
-    }
-}
+    function overTheRoad($address, $street){
+        for($i=)
+        return 4;
+    }}
 
 
 echo "<pre>";
-print_r((new OverTheRoad)->past(2, 8, 20));
+print_r((new OverTheRoad)->overTheRoad(2, 8));
 echo "</pre>";
