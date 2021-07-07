ed7d6d0e90cd7e023c0bf2669ec4ae38adfc18c2
Your Name <you@example.com>
2021-07-08 00:01:09 +0430

    Quarteroftheyear

--- src/OverTheRoad.php
+++ src/OverTheRoad.php
@@ -6,11 +6,18 @@ class OverTheRoad
 {
 
     function overTheRoad($address, $street){
-        for($i=)
-        return 4;
+        for ($x = 0; $x < $street ; $x++) {
+            $ar1[$x] = ($x+1)*2-1 ;
+        }
+        for ($x = 0; $x < $street ; $x++) {
+            $ar1[$x] = ($x+1)*2 ;
+        }
+
+
+        return $ar1;
     }}
 
 
 echo "<pre>";
-print_r((new OverTheRoad)->overTheRoad(2, 8));
+print_r((new OverTheRoad)->overTheRoad(2, 3));
 echo "</pre>";
