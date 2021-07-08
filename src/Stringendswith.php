2723c059a885b769dd3ba3575355a30fdf513dfc
Your Name <you@example.com>
2021-07-08 16:53:04 +0430

    Quarteroftheyear

--- src/Stringendswith.php
+++ src/Stringendswith.php
@@ -4,14 +4,27 @@ namespace saeed;
 
 class Stringendswith
 {
-    function solution($str, $ending)
+    // My solution:
+    // function solution($str, $ending)
+    // {
+
+    // How can I get the last 7 characters of a PHP string?
+    // https://stackoverflow.com/questions/10542310/how-can-i-get-the-last-7-characters-of-a-php-string
+    // if ((substr($str, -strlen($ending)) == $ending) || $ending == "")
+    //    return true;
+    // }
+
+
+    // Codewars solution:
+    function solution($str, $ending): bool
     {
-        return 3;
+        return substr($str, -strlen($ending), strlen($ending)) == $ending;
     }
+
 
 }
 
 
 echo "<pre>";
-print_r((new Stringendswith)->solution(5, 6));
+print_r((new Stringendswith)->solution("samurai", "ai"));
 echo "</pre>";
