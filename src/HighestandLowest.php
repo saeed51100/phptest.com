96f23b58ef4e0b0df49002e81649ddeb3ec624e9
Your Name <you@example.com>
2021-07-08 22:38:59 +0430

    Quarteroftheyear

--- src/HighestandLowest.php
+++ src/HighestandLowest.php
@@ -7,9 +7,7 @@ class HighestandLowest
     function highAndLow($numbers)
     {
         $ar1 = explode(" ", $numbers);
-
         sort($ar1);
-
         foreach ($numbers as $key => $val) {
             $ar1[$key] = $val;
         }
@@ -21,5 +19,5 @@ class HighestandLowest
 
 
 echo "<pre>";
-print_r((new HighestandLowest)->highAndLow("8 3 -5 42 -1  0 0 -9 4 7 4 -4"));
+print_r((new HighestandLowest)->highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
 echo "</pre>";
