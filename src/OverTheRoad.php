632002cc5695144661d872db69231f755415d534
Your Name <you@example.com>
2021-07-08 15:20:15 +0430

    Quarteroftheyear

--- src/OverTheRoad.php
+++ src/OverTheRoad.php
@@ -4,26 +4,33 @@ namespace saeed;
 
 class OverTheRoad
 {
-
     function overTheRoad($address, $street)
     {
-        for ($x = 0; $x < $street; $x++) {
-            $ar1[$x] = 2 * $x + 1;
-        }
-        for ($x = 0; $x < $street; $x++) {
-            $ar2[$x] = 2 * $x + 2;
-        }
 
-        $ar2 = array_reverse($ar2);
+        // My solution1:this is not working for big arrays.
+        //     for ($x = 0; $x < $street; $x++) {
+        //         $ar1[$x] = 2 * $x + 1;
+        //     }
+        //     for ($x = 0; $x < $street; $x++) {
+        //         $ar2[$x] = 2 * $x + 2;
+        //     }
 
-        if (in_array($address, $ar1))
-            return $ar2[array_search($address, $ar1)];
-        else return $ar1[array_search($address, $ar2)];
+        //     $ar2 = array_reverse($ar2);
+
+        //     if (in_array($address, $ar1))
+        //         return $ar2[array_search($address, $ar1)];
+        //     else return $ar1[array_search($address, $ar2)];
+
+
+        // My solution2: :)
+        return $street * 2 - $address + 1;
 
     }
+
+
 }
 
 
 echo "<pre>";
-print_r((new OverTheRoad)->overTheRoad(250, 1005000));
+print_r((new OverTheRoad)->overTheRoad(5, 6));
 echo "</pre>";
