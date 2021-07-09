de94cdad05f7479f83710dda4df8b8311997005d
Your Name <you@example.com>
2021-07-09 13:24:59 +0430

    Quarteroftheyear

--- src/ScalingSquaredStrings.php
+++ src/ScalingSquaredStrings.php
@@ -4,12 +4,24 @@ namespace saeed;
 
 class ScalingSquaredStrings
 {
-    function scale($s, $k, $n) {
-        return 4;
+    function scale($s, $k, $n)
+    {
+
+//        $ar1 = explode("\n", $s);
+//        $st = str_split($ar1[0]);
+
+        function countLetters($matches, $k): string
+        {
+            return $matches[0] . '(' . strlen($matches[0]) . ')';
+//            return str_repeat($matches[0],$k);
+        }
+
+
+        return preg_replace_callback('/[CG]/', 'countLetters',$s);
+
+
     }
 }
-
-
 
 
 echo "<pre>";
