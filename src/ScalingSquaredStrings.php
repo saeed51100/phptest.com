05bc6d9f75002749729a4f3eeeddd9c04691b1cd
Your Name <you@example.com>
2021-07-09 20:50:04 +0430

    Quarteroftheyear

--- src/ScalingSquaredStrings.php
+++ src/ScalingSquaredStrings.php
@@ -7,23 +7,23 @@ class ScalingSquaredStrings
     function scale($s, $k, $n)
     {
 
-//        $ar1 = explode("\n", $s);
-//        $st = str_split($ar1[0]);
-
 
 //        return str_repeat($s,$k);
 
         // use ($k) ????
         // Passing additional arguments to preg_replace_callback using PHP 5.2.6
         //https://stackoverflow.com/questions/9550769/passing-additional-arguments-to-preg-replace-callback-using-php-5-2-6?noredirect=1&lq=1
-        return preg_replace_callback('/[a-zA-Z]+/',
+        $yyy = preg_replace_callback('/[a-zA-Z]/',
             function ($matches) use ($k) {
-//                return $matches[0] . '(' . strlen($matches[0]) . ')';
                 return str_repeat($matches[0], $k);
             },
             $s);
 
-
+        return preg_replace_callback('/[a-zA-Z]+/',
+            function ($matches) use ($n) {
+                return str_repeat($matches[0], $n);
+            },
+            $yyy);
 
 
     }
@@ -31,5 +31,5 @@ class ScalingSquaredStrings
 
 
 echo "<pre>";
-print_r((new ScalingSquaredStrings)->scale("CG\nla", 3, 3));
+print_r((new ScalingSquaredStrings)->scale("CG\nla", 3, 8));
 echo "</pre>";
