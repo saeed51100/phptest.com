97434ad8645cb2db594f09171f68330362c7fc47
Your Name <you@example.com>
2021-07-10 20:31:53 +0430

    Quarteroftheyear

--- src/OddorEven.php
+++ src/OddorEven.php
@@ -8,7 +8,7 @@ class OddorEven
     {
         // My solution:
         // https://stackoverflow.com/questions/7959247/test-if-number-is-odd-or-even
-        if (array_sum($a) % 2 == 0 || $a == 0)
+        if (array_sum($a) % 2 == 0)
             return "even";
         else return "odd";
 
