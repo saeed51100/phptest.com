3839036311f1d4302e104bf2ac63705135112528
Your Name <you@example.com>
2021-07-11 23:36:06 +0430

    Quarteroftheyear

--- src/Findtheoddint.php
+++ src/Findtheoddint.php
@@ -5,16 +5,27 @@ namespace saeed;
 class Findtheoddint
 {
 
-    function findIt(array $seq) /*: int*/
+    function findIt(array $seq): int
     {
-        $y = array_count_values($seq);
-//        return  $y;
-        foreach ($y as $value) {
-            if ($value % 2 !== 0)
-                return [array_search($value)];
+        // My solution:
+        // $y = array_count_values($seq);
+        // foreach ($y as $value) {
+        //     if ($value % 2 !== 0)
+        //         return array_search($value, $y);
+        // }
 
+
+        // Codewars solution:
+        $nums = array_count_values($seq);
+        foreach ($nums as $key => $val) {
+            if ($val % 2) {
+                return $key;
+            }
         }
+
+
     }
+
 
 }
 
