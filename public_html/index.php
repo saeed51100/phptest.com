c4dda8d87a5b3f275b9f17fa512a5002e68e6179
Your Name <you@example.com>
2021-07-11 23:51:42 +0430

    Quarteroftheyear

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,4 @@
 <?php
-require '../src/Findtheoddint.php';
+require '../src/Sorttheodd.php';
 
 
---
+++ src/Sorttheodd.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace saeed;
+
+class Sorttheodd
+{
+
+    function sortArray(array $arr) : array {
+        // Return a sorted array.
+    }
+
+}
+
+
+echo "<pre>";
+print_r((new Sorttheodd)->sortArray([5, 3, 2, 8, 1, 4]));
+echo "</pre>";
+
+
+
+//echo(new Sorttheodd)->generateShape(4);
+
+
+
+
