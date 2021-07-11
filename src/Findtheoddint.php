c33e1b4ec01616ea035648f4a93d758da55332e0
Your Name <you@example.com>
2021-07-11 23:15:32 +0430

    Quarteroftheyear

--- src/Findtheoddint.php
+++ src/Findtheoddint.php
@@ -7,15 +7,20 @@ class Findtheoddint
 
     function findIt(array $seq) /*: int*/
     {
-        return array_count_values($seq);
-        foreach ()
+        $y = array_count_values($seq);
+//        return  $y;
+        foreach ($y as $value) {
+            if ($value % 2 !== 0)
+                return [array_search($value)];
+
+        }
     }
 
 }
 
 
 echo "<pre>";
-print_r((new Findtheoddint)->findIt([20,1,1,2,2,3,3,5,5,4,20,4,5]));
+print_r((new Findtheoddint)->findIt([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5]));
 echo "</pre>";
 
 
