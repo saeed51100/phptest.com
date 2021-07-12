26e71a5d5f1c597989c4bccc705f44947fc27517
Your Name <you@example.com>
2021-07-12 21:44:10 +0430

    Quarteroftheyear

--- src/Sorttheodd.php
+++ src/Sorttheodd.php
@@ -5,13 +5,16 @@ namespace saeed;
 class Sorttheodd
 {
 
-    function sortArray(array $arr): array
+    function sortArray(array $arr)
     {
-        foreach ($arr as $key => $value)
+        foreach ($arr as $key => $val) {
+            if ($val % 2) {
+                echo $key;
+            }
+        }
+
     }
-
 }
-
 
 echo "<pre>";
 print_r((new Sorttheodd)->sortArray([5, 3, 2, 8, 1, 4]));
