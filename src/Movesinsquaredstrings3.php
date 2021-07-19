78c05ecdd9bb1a21c1457f99d3595a7eeb7b8f3d
saeed <saeed@ddd.com>
2021-07-19 18:59:07 +0430

    Array.diff

--- src/Movesinsquaredstrings3.php
+++ src/Movesinsquaredstrings3.php
@@ -3,63 +3,65 @@
 namespace saeed;
 
 
+function diag1Sym($s)
+{
+    $a = explode("\n", $s);
+    foreach ($a as $key => $value) {
+        foreach ($a as $keya => $valuea) {
+            $zz1[$key][$keya] = $a[$keya][$key];
 
-    function diag1Sym($s)
-    {
-
-        $a = explode("\n", $s);
-        foreach ($a as $key => $value) {
-            foreach ($a as $keya => $valuea) {
-                $zz1[$key][$keya] = $a[$keya][$key];
-
-            }
         }
-
-
-        foreach ($zz1 as $value) {
-            $output[] = implode('', $value);
-        }
-
-        return implode("\n", $output);
-
-
     }
 
 
-    function rot90Clock($s)
-    {
+    foreach ($zz1 as $value) {
+        $output[] = implode('', $value);
+    }
+
+    return implode("\n", $output);
+
+
+}
+
+
+function rot90Clock($s)
+{
 //        return $s;
-//        $s = diag1Sym($s);
-        return implode("\n", array_reverse(explode("\n", strrev(diag1Sym($s)))));
+        $s = diag1Sym($s);
+    return implode("\n", array_reverse(explode("\n", strrev($s))));
+}
+
+
+function selfieAndDiag1($s)
+{
+    $a = explode("\n", $s);
+
+    $imp = explode("\n", diag1Sym($s));
+    foreach ($a as $key => $value) {
+        $output2[$key] = $a[$key] . "|" . $imp[$key];
     }
 
-
-    function selfieAndDiag1($s)
-    {
-        $a = explode("\n", $s);
-
-        $imp = explode("\n", diag1Sym($s));
-        foreach ($a as $key => $value) {
-            $output2[$key] = $a[$key] . "|" . $imp[$key];
-        }
-
-        return implode("\n", $output2);
-    }
+    return implode("\n", $output2);
+}
 
 
-    // Codewars function:
-    function oper($fct, $s)
-    {
-        return $fct($s);
-    }
+// Codewars function:
+function oper($fct, $s)
+{
+    return $fct($s);
+}
 
-
+//echo "<pre>";
+//print_r(rot90Clock("abcd\nefgh\nijkl\nmnop")) ;
+//echo "</pre>";
 
 
 echo "<pre>";
-oper('rot90Clock', "abcd\nefgh\nijkl\nmnop");
+print_r(oper('rot90Clock',"abcd\nefgh\nijkl\nmnop")) ;
 echo "</pre>";
 
-//print_r((new Movesinsquaredstrings3)->sortArray([5, 3, 2, 8, 1, 4]));
+
+
+
 
 
