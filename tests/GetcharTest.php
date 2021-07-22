5ceaba3e724ec4a737a19350f9f07825fcc67c96
Your Name <you@example.com>
2021-07-22 22:09:54 +0430

    Quarteroftheyear

--- tests/GetcharTest.php
+++ tests/GetcharTest.php
@@ -2,22 +2,20 @@
 
 use PHPUnit\Framework\TestCase;
 
-
 class GetcharTest extends TestCase
 {
 
     public function setUp(): void
     {
-        if (isset($_POST['getChar'])) {
-            unset($_POST['getChar']);
-        }
+        require '../src/Getchar.php';
+
+        $this->assertTrue(true);
     }
 
     public function testSampleTests()
     {
-        $_POST['getChar'] = 'bar';
-        ob_start();
-        include('../src/Getchar.php');
+
+
         $this->assertEquals('A', getChar(65));
 
     }
