4cbcf0324f7beb177cc32b0f7c60244a76bb133e
Your Name <you@example.com>
2021-07-22 22:27:07 +0430

    Quarteroftheyear

--- tests/GetcharTest.php
+++ tests/GetcharTest.php
@@ -1,13 +1,15 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+require '../src/Getchar.php';
 
 class GetcharTest extends TestCase
 {
 
+    private string $getChar;
+
     public function setUp(): void
     {
-        require '../src/Getchar.php';
 
         $this->assertTrue(true);
     }
