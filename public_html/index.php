6615bda1b304a5f7a12ffc7037872c806d84e11b
saeed <saeedblock77@gmail.com>
2022-06-03 12:20:52 +0430

    khoone 1401/03/13

--- public_html/index.php
+++ public_html/index.php
@@ -2,9 +2,16 @@
 
 function periodIsLate($last, $today, $cycleLength)
 {
+$aaa=date_create($today);
+$bbb=date_create($last);
+
+if ((date_diff($aaa, $bbb) ->days)>$cycleLength){
+    return 1;
+}
 
 
-    return ((date_diff(date_create($today), date_create($last))->days) > $cycleLength);
+
+//    return ((date_diff(date_create($today), date_create($last))->days) > $cycleLength);
 
 
 }
@@ -14,4 +21,4 @@ function periodIsLate($last, $today, $cycleLength)
 //print_r(periodIsLate("2017-07-13", "2017-07-11", 5));
 //echo "</pre>";
 
-echo(periodIsLate("2017-07-13", "2017-07-11", 4));
+echo(periodIsLate("2017-07-18", "2017-07-11", 10));
