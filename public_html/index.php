3476a457434806cdc513d729d0de1374a5d2f8f1
saeed <saeedblock77@gmail.com>
2022-06-07 21:54:11 +0430

    khoone 1401/03/16

--- public_html/index.php
+++ public_html/index.php
@@ -1,16 +1,25 @@
 <?php
 
-function summation($c)
-{
-//     I find this in internet:
-     return ($c * ($c + 1) / 2);
 
-//    return array_sum(range(1, $c));
+function quarterOf($month)
+{
+//        if ($month == 1 || $month == 2 || $month == 3) {
+//            return 1;
+//        }
+//        if ($month == 4 || $month == 5 || $month == 6) {
+//            return 2;
+//        }
+//        if ($month == 7 || $month == 8 || $month == 9) {
+//            return 3;
+//        }
+//        if ($month == 10 || $month == 11 || $month == 12) {
+//            return 4;
+//        } else {
+//            return "enter 1-12";
+//        }
+    return ceil($month / 3);
+
 }
 
-
-
-
-echo summation(8);
-
+echo (new Quarteroftheyear)->quarterOf(7);
 
