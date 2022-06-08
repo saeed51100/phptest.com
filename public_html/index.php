cc427d53f982e847c2f21a7958b8a4ec57963702
saeed <saeedblack77@gmail.com>
2022-06-08 14:30:47 +0430

    edare 1401/03/18

--- public_html/index.php
+++ public_html/index.php
@@ -1,25 +1,23 @@
 <?php
 
-
-function quarterOf($month)
+function basicOp($op, $val1, $val2)
 {
-//        if ($month == 1 || $month == 2 || $month == 3) {
-//            return 1;
-//        }
-//        if ($month == 4 || $month == 5 || $month == 6) {
-//            return 2;
-//        }
-//        if ($month == 7 || $month == 8 || $month == 9) {
-//            return 3;
-//        }
-//        if ($month == 10 || $month == 11 || $month == 12) {
-//            return 4;
-//        } else {
-//            return "enter 1-12";
-//        }
-    return ceil($month / 3);
-
+    switch ($op) {
+        case "+":
+            return $val1 + $val2;
+        case "-":
+            return $val1 - $val2;
+        case "*":
+            return $val1 * $val2;
+        case "/":
+            return $val1 / $val2;
+        default:
+            return "no match operator";
+    }
 }
 
-echo quarterOf(7);
+echo basicOp('/', 4, 7);
+
+
+
 
