f117018104b318bbf479b4fc9b472bf1e2072380
saeed <saeedblock77@gmail.com>
2022-06-09 23:16:52 +0430

    khoone 1401/03/19

--- public_html/index.php
+++ public_html/index.php
@@ -1,9 +1,14 @@
 <?php
 
 
-function reverse($c): string
-{
-   return implode(' ', array_reverse(explode(' ', strrev($c)))) ;
+function solution($c): string
+{   // I find this:
+    // https://www.delftstack.com/howto/php/how-to-properly-format-a-number-with-leading-zeros-in-php/
+     return "Value is " . str_pad($c, 5, "0", STR_PAD_LEFT);
+
+//    return sprintf("Value is %05d",$c); // This in codewars.
+
 }
 
-echo reverse("ehT kciuq nworb xof spmuj revo eht yzal .god");
+echo (solution("5"));
+
