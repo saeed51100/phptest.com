8950323451c8cb4cc3d44acba8d349075f1b0b7d
saeed <saeedblock77@gmail.com>
2022-06-10 22:27:51 +0430

    khoone 1401/03/20

--- public_html/index.php
+++ public_html/index.php
@@ -1,14 +1,12 @@
 <?php
 
 
-function solution($c): string
-{   // I find this:
-    // https://www.delftstack.com/howto/php/how-to-properly-format-a-number-with-leading-zeros-in-php/
-     return "Value is " . str_pad($c, 5, "0", STR_PAD_LEFT);
-
-//    return sprintf("Value is %05d",$c); // This in codewars.
-
+function overTheRoad($address, $street)
+{
+    return $street * 2 - $address + 1;
 }
 
-echo (solution("5"));
 
+echo "<pre>";
+print_r(overTheRoad(5, 6));
+echo "</pre>";
