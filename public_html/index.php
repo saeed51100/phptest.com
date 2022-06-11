d5478a1b63325f5ddb0c1e1e7b7395f052a295e5
saeed <saeedblock77@gmail.com>
2022-06-12 00:14:51 +0430

    khoone 1401/03/20

--- public_html/index.php
+++ public_html/index.php
@@ -2,21 +2,21 @@
 
 function highAndLow($numbers)
 {
-//     My solution:
-     $ar1 = explode(" ", $numbers);
-     sort($ar1);
-     foreach ($ar1 as $key => $val) {
-         $ar1[$key] = $val;
-     }
-     return $ar1[count($ar1) - 1] . " " . $ar1[0];
+
+//    $ar1 = explode(" ", $numbers);
+//    sort($ar1);
+//    foreach ($ar1 as $key => $val) {
+//        $ar1[$key] = $val;
+//    }
+//    return $ar1[count($ar1) - 1] . " " . $ar1[0];
 
 
     // Codewars solution:
-//    $a = explode(' ', $numbers);
-//    return max($a) . " " . min($a);
+    $a = explode(' ', $numbers);
+    return max($a) . " " . min($a);
 
 
 }
 
 
-echo (highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+echo(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
