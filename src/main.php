e2c57e05b74f3d738a5472c6d3b9c23411512c40
saeed <saeedblack77@gmail.com>
2023-12-09 23:10:55 +0330

    khooneh

--- src/main.php
+++ src/main.php
@@ -1,20 +1,13 @@
 <?php
-function vertMirror($s)
-{
-    // How to explode a multi-line string?
-    // https://stackoverflow.com/questions/6162912/how-to-explode-a-multi-line-string
-    return implode("\n", array_reverse(explode("\n", strrev($s))));
-}
-function horMirror($s)
-{
-    return implode("\n", array_reverse(explode("\n", $s)));
-}
-function oper($fct, $s)
-{
-    if ($fct == 'horMirror')
-        return horMirror($s);
-    else
-        return vertMirror($s);
-}
-
-print_r(oper('horMirror', "abcd\nefgh\nijkl\nmnop\n"));
+$a = 32;
+echo "a is " . is_numeric($a) . "\n";
+$b = 0;
+echo "b is " . is_numeric($b) . "\n";
+$c = 32.5;
+echo "c is " . is_numeric($c) . "\n";
+$d = "32";
+echo "d is " . is_numeric($d) . "\n";
+$e = true;
+echo "e is " . is_numeric($e) . "\n";
+$f = null;
+echo "f is " . is_numeric($f) . "\n";
